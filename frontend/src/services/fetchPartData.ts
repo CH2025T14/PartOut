af7d2e74fa296a841d7764e195aa0dab2270777d
Geesing <chewgeesing@gmail.com>
2025-03-21 20:57:31 -0400

    First stab at API call (with env API key)

---
+++ frontend/src/services/fetchPartData.ts
@@ -0,0 +1,43 @@
+export async function getParts(setNum: number) {
+  interface ApiResponseItem {
+    part: Part;
+    quantity: number;
+  }
+
+  interface Part {
+    part_num: string;
+    name: string;
+    part_img_url: string;
+  }
+
+  const url = "https://rebrickable.com/api/v3/lego/sets/" + setNum + "-1/parts/";
+  const apiKey = import.meta.env.VITE_REBRICKABLE_API_KEY;
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Authorization": `key ${apiKey}`,
+        "Content-Type": "application/json"
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const apiData = (await response.json()).results;
+    const partData = apiData.map((item: ApiResponseItem) => {
+      return {
+        partName: item.part.name,
+        partId: item.part.part_num,
+        targetQty: item.quantity,
+        currQty: 0,
+        imgUrl: item.part.part_img_url,
+      };
+    });
+    return partData;
+  } catch (error) {
+    console.error("Error fetching parts:", error);
+  }
+}
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PUBLIC_URL: string;
+  readonly VITE_BACKEND_URL: string;
+  readonly VITE_REBRICKABLE_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
