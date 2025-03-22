c2ae16975b7699c8f8a6586b8634c5269a9df40b
Daniel (Juyeong) Kim <kjy11gold@gmail.com>
2025-03-21 20:45:12 -0400

    Add icona

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,29 @@
 import { Input } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import React from 'react';
+import './home.css';
 
-export default function Home() {
+const { Search } = Input;
+
+export default function Home(): React.ReactElement {
+
   return (
     <div>
-      <h1>PartOut</h1>
-      <Input placeholder="Enter your set number(s)" />
+      <h1>PartOut Logo Here..</h1>
+      <Search
+        placeholder="Enter your set number(s)"
+        allowClear
+        enterButton
+        size="large"
+      />
+      <p className="dividerText">Or</p>
+      <div>
+        {/* <Link to="/msclassify"> */}
+          <button className="openBtn">
+            Open <UploadOutlined />
+          </button>
+        {/* </Link> */}
+      </div>
     </div>
   );
 }
