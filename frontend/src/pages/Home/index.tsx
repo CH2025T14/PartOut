9169f4be1333f80884d79aeef19eb01a1e979c44
Daniel (Juyeong) Kim <kjy11gold@gmail.com>
2025-03-22 00:53:36 -0400

    Styling

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Input, Image, Empty } from 'antd';
-import { UploadOutlined, PlusOutlined } from '@ant-design/icons';
+import { UploadOutlined, PlusOutlined, BorderOuterOutlined, CalendarOutlined, UnorderedListOutlined } from '@ant-design/icons';
 import { Link, Outlet } from "react-router";
 import React from 'react';
 import { getSetData, Set } from '../../services/fetchSetData';
@@ -28,9 +28,9 @@ export default function Home(): React.ReactElement {
   }
 
   return (
-    <div>
-      <div className="logo">
-        <h1>PartOut Logo Here..</h1>
+    <div className="homeContainer">
+      <div className="logoContainer">
+        <BorderOuterOutlined className="mainLogo" /><h1>PartOut</h1>
       </div>
 
       <Search
@@ -40,6 +40,7 @@ export default function Home(): React.ReactElement {
         size="large"
         onSearch={onSearch}
         className="searchInput"
+        style={{ width: '70%', maxWidth: '600px', margin: '0 auto' }}
       />
       <p className="dividerText">Or</p>
       <div>
@@ -51,17 +52,20 @@ export default function Home(): React.ReactElement {
         {setData && (
           <div className="setDataThumbnail">
             <Link to="/set_page">
-              <button className="addSetBtn"><PlusOutlined /></button>
+              <button className="addSetBtn"><PlusOutlined className="addBtnIcon"/></button>
             </Link>
             <Image
-              width={200}
               src={setData.setImgUrl}
               alt={setData.name}
+              preview={false}
+              style={{ objectFit: 'contain', maxHeight: '120px' }}
             />
             <div className="setDataMeta">
               <h2>{setData.name}</h2>
-              <p>Released: {setData.year}</p>
-              <p>{setData.numParts} parts</p>
+              <div className="setDataDetails">
+                <p><CalendarOutlined /> Released: {setData.year}</p>
+                <p><UnorderedListOutlined /> {setData.numParts} parts</p>
+              </div>
             </div>
           </div>
         )}
